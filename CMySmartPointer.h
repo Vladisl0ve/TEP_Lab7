fbc7262565e40998102f74575e264ca1bbdb2b10
Vladislove <vladislav.lapko@gmail.com>
2019-12-02 11:15:20 +0100

    Add project files.

---
+++ CMySmartPointer.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "CRefCounter.h"
+
+template <typename T>
+class CMySmartPointer
+{
+public:
+	CMySmartPointer(T* pcPointer)
+	{
+		pc_pointer = pcPointer;
+		pc_counter = new CRefCounter();
+		pc_counter->iAdd();
+	}//CMySmartPointer(T *pcPointer)
+	CMySmartPointer(const CMySmartPointer& pcOther)
+	{
+		pc_pointer = pcOther.pc_pointer;
+		pc_counter = pcOther.pc_counter;
+		pc_counter->iAdd();
+	}//CMySmartPointer(const CMySmartPointer &pcOther)
+	~CMySmartPointer()
+	{
+		if (pc_counter->iDec() == 0)
+		{
+			delete pc_pointer;
+			delete pc_counter;
+		}//if (pc_counter->iDec())
+	}//~CMySmartPointer()
+	T& operator*() { return(*pc_pointer); }
+	T* operator->() { return(pc_pointer); }
+private:
+	CRefCounter* pc_counter;
+	T* pc_pointer;
+};//class CMySmartPointer
+
